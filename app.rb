f0c7cbcdab95066e5f594d38da8e26c7a2e2fdc9
Student <student@makerss-air-2.mynet>
2020-06-03 17:58:44 +0100

    refactored dummy data insertion in view_bookmark_spec.rb

--- app.rb
+++ app.rb
@@ -4,22 +4,19 @@ require 'sinatra/base'
 require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-  # get '/b' do
-  #   erb :index
-  # end
 
   get '/' do
     @bookmarks = Bookmark.all
     erb :index
   end
 
+  get '/add_bookmarks' do
+    erb :add_bookmark
+  end
+
   post '/save_url' do
     Bookmark.create(url: params[:url])
     redirect '/'
-  end
-
-  get '/add_bookmarks' do
-    erb :add_bookmark
   end
 
   run! if app_file == $PROGRAM_NAME
